52a53fec1d88fdd5eda8a2f67cd5d5c8e20c62fb
rgiles4 <ryan.giles@maine.edu>
2025-09-18 23:36:58 -0400

    Register commands through RESTful endpoint

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 import dotenv from 'dotenv';
+import { registerCommands } from './lib/api.js';
 import { Client, GatewayIntentBits } from 'discord.js';
+import { commands } from './lib/commands.js';
 
 dotenv.config();
+
+const clientId = process.env.CLIENT_ID;
+if (!clientId) {
+    throw new Error('CLIENT_ID is not defined in environment variables.');
+}
+const botToken = process.env.BOT_TOKEN;
+if (!botToken) {
+    throw new Error('BOT_TOKEN is not defined in environment variables.');
+}
+const guildId = process.env.GUILD_ID;
+if (!guildId) {
+    throw new Error('GUILD_ID is not defined in environment variables.');
+}
+
+const commandMap = new Map(commands.map(cmd => [cmd.data.name, cmd])); // Map Commands to Handler
 const client = new Client({ intents:
     [
         GatewayIntentBits.Guilds,
@@ -10,14 +27,21 @@ const client = new Client({ intents:
     ]
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(botToken);
 
 client.on('clientReady', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    registerCommands(); // Register commands on startup
 });
 
-client.on('messageCreate', msg => {
-    if (msg.content === '!ping') {
-        msg.reply('pong');
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const command = commandMap.get(interaction.commandName);
+    if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error('Error executing command:', error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
